1f635fcb3ed743f827b112ce752622fcb5d0172d
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2025-02-14 19:24:36 -0500

    se agrego retornar si existe el botton siguiente o algun elemento de paginacion, eso servira para recorrer pagina con paginacion xd, valga la redundancia

--- src/scrape.py
+++ src/scrape.py
@@ -3,7 +3,8 @@ from pprint import pprint
 from bs4 import BeautifulSoup
 
 headersxd = {
-    'targer': ".product-miniature.js-product-miniature"
+    'targer': ".product-miniature.js-product-miniature",
+    'targer_pagination': '.next.js-search-link'
 }
 
 struct_data = {
@@ -26,25 +27,41 @@ def get_page(url: str):
     soup = BeautifulSoup(response.text, "html.parser")
     return soup
 
-def get_items(soup: BeautifulSoup, target: str, struct_data: dict) -> list:
+def get_items(soup: BeautifulSoup, target: str, target_pagination: str) -> tuple[list, bool]:
     select_items = soup.select(target)
     items_list = []
     for item in select_items:
         value_item =  item.get_text(strip=True, separator='\n')
-        
         items_list.append(value_item)
-    return items_list
+        
+    select_pagination = soup.select(target_pagination)
+    
+    empty_pagination = True if not select_pagination else False
+    
+    print(empty_pagination, type(empty_pagination))
+    return items_list, empty_pagination
 
 
 def main():
     urlx = 'https://www.sercoplus.com/545-video-nvidia-geforce-rtx'
-    url = 'https://www.sercoplus.com/266-PromoPlus'
-    target = '.product-miniature.js-product-miniature'
-    page_result = get_page(url)
-    data_result = get_items(page_result, target, struct_data)
-    for data in data_result:
-        print(data)
-        print(type(data), '\n,\n\n')
+    url = 'https://www.sercoplus.com/266-PromoPlus?page='
+    num_page = 1
+    empty_pagination = False
+    target_group_items = '.product-miniature.js-product-miniature'
+    target_pagination = '.next.js-search-link'
+    while not empty_pagination:
+        
+        new_url = f'{url}{num_page}'
+        page_result = get_page(new_url)
+        data_result, empty_pagination = get_items(page_result, target_group_items, target_pagination)
+        
+        print(f'link de la pagina: {new_url}')
+        
+        for data in data_result:
+            print(data)
+            print(type(data), '\n,\n\n')
+            
+        num_page = num_page + 1
 
 if '__main__' == __name__:
     main()
